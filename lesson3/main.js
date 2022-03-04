a2189350a499108e814641845d6017e1fa086d22
Firdavs <firdavssherzodovic@gmail.com>
2022-03-04 21:50:49 +0500

    2masala

---
+++ lesson3/main.js
@@ -0,0 +1,97 @@
+// document.write('Men sevaman O`zbekistonni')
+// alert('Men')
+// // sonlar
+// console.log("Siz kiritgan son 27");
+// console.log('1.13 49');
+// console.log('7.16   100');
+// console.log('3 6 9 4');
+// console.log('5');
+// console.log('10');
+// console.log('21');
+// console.log('1 2');
+// console.log('3 4');
+// console.log('5 10');
+// console.log('7sm');
+// console.log(14 ** 5);
+// console.log(5 ** 4);
+// console.log(5 ** 6);
+// console.log(((6-3)*2) ** 2);
+
+let x = 5
+y = 7 * x + 3 * x + 6
+console.log(y);
+
+let a = 4 
+x = 12 * a + 7 * a + 12
+console.log(x);
+
+a = 10 
+p = a * 4    
+console.log(p);
+
+let r = 12
+d = r * 2 
+console.log(d);
+ 
+r = 5
+let l = 2 * Math.PI * r
+console.log(l);
+
+let s = Math.PI * r * r
+console.log(s);
+
+a = 2
+let b = 4
+let e = ((a + b) / 2)
+console.log(e);
+
+let g = ((a * b) ** 1/2)
+console.log(g);
+
+a = 6 
+b = 12 
+p = ((a + b) * 2) 
+console.log(p);
+
+s = a * b
+console.log(s);
+
+a = 12 
+b = 10 
+d = a * b
+console.log(d);
+
+a = 6 
+b = 8 
+g = a * b
+console.log(g);
+
+let k = d / g
+console.log(k);
+
+let kg = 15000
+b = kg * 3
+console.log(b);
+
+// baholari
+let o = 15000
+p = 10000 
+k = 25000 
+// kilogramlari
+x = prompt('neca kilo olma')
+y = prompt('neca kilo pecci')
+z = prompt('neca kilo kofet')
+// qowimca
+let ox = o * x
+let py = p * y  
+let kz = k * z
+// summasi
+console.log('Olma:' ,ox);
+console.log('Pecci:' ,py);
+console.log('Kofet:' ,kz);
+console.log('Jami:' ,ox + py +kz);
+// document.write
+document.write('Olma:' ,ox , '<br>');
+document.write('Pecci:' ,py , '<br>');
+document.write('Kofet:' ,kz , '<br>');
+document.write('Jami:' ,ox + py +kz ,'<br>');
