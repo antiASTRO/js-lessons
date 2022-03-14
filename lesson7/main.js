b2d68ae294df6b24c98abd820e79b2f401f46d0f
Firdavs <firdavssherzodovic@gmail.com>
2022-03-14 21:56:32 +0500

    if urgandik

---
+++ lesson7/main.js
@@ -0,0 +1,13 @@
+
+let a = +prompt('a ni kiriting')
+let b = +prompt('b ni kiriting')
+
+if (a > b) {
+    console.log('a katta');
+}
+else if (a == b) {
+    console.log('durrang');
+}
+else {
+    console.log('b katta');
+}
