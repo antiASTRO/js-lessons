5a5c77500ae1ceed1508b1d326e5f7b28b026b73
Firdavs <firdavssherzodovic@gmail.com>
2022-03-18 21:53:29 +0500

    arrey organdik

---
+++ lesson9/main.js
@@ -0,0 +1,45 @@
+
+// let arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+// console.log(arr[2] + arr[7])
+
+// let persons = ['Asadbek', 'Azamat', 'Firdavs', 'Ogabek', 'Shox', 'Jahongir', 'Kenjaev' ]
+
+// let jobs = ['Uchuvchi', 'Quruvchi', 'Militsioner', 'Pensioner', 'Dasturchi', 'O`qituvchi', 'Bekorchi', 'Dangasa']
+
+// let years = [3, 2, 4, 5, 1]
+
+// console.log(persons[6], jobs[4], 'bolib', years[3], 'yildan beri ishlaydi');
+
+// console.log(persons[0], jobs[5], 'bolib', years[2], 'yildan beri ishlaydi');
+
+// console.log(persons[2], jobs[0], 'bolib', years[1], 'yildan beri ishlaydi');
+
+// console.log(persons[5], jobs[7], 'bolib', years[3], 'yildan beri ishlaydi');
+
+// console.log(persons[1], jobs[2], 'bolib', years[0], 'yildan beri ishlaydi');
+
+// console.log(`${persons[3]} ${jobs[4]} bolib ${years[3]} yildan beri ishlaydi`);
+
+// console.log(`${persons[4]} ${jobs[7]} bolib ${years[3]} yildan beri ishlaydi`);
+
+
+let ns = [0, 0, 0, 0, 0]
+
+ns[0] = +prompt('1-son kirit:')
+ns[1] = +prompt('2-son kirit:')
+ns[2] = +prompt('3-son kirit:')
+ns[3] = +prompt('4-son kirit:')
+ns[4] = +prompt('5-son kirit:')
+
+console.log(ns);
+
+
+
+
+
+
+
+
+
+
