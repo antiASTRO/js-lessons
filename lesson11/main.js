23ef5e8e40153025da30e2488b627de0076c4e03
Firdavs <firdavssherzodovic@gmail.com>
2022-03-25 21:55:58 +0500

    for otdik

---
+++ lesson11/main.js
@@ -0,0 +1,23 @@
+// for (let i = 1; i <= 100; i++){
+
+//     if (i * 2 === 88) {
+//         console.log('Shoqqi LGBT');
+//     }
+//     else {
+//         console.log(i * 2);
+//     }
+//     }
+
+// For sikl
+// let k = 10; 
+// let n = 5;
+// for (i=0; i < n; i++) {
+//     console.log(k);
+// }
+
+let a = 13; 
+let b = 22;
+for (i=a; i <= b; i++) {
+    console.log(i);
+}
+console.log('Sonlar soni:',b-a);
